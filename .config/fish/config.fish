a2441d8be42e29ca66e6ef8069090ef5f0b0bcf0
Riley Flynn <riley@rileyflynn.me>
2019-06-21 20:39:46 -0230

    Fancy screenshot time

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,5 +2,6 @@
 
 set -x PYENV_ROOT $HOME/.pyenv
 set -x PATH $PYENV_ROOT/bin $PATH
+set -x PATH $HOME/.scripts $PATH
 status --is-interactive; and . (pyenv init -|psub)
 status --is-interactive; and . (pyenv virtualenv-init -|psub)
