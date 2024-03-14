2b164fafbccafc0b0d64e55e0a9ea68d8118b15c
Riley Flynn <riley@rileyflynn.me>
2024-03-14 11:54:44 -0230

    Automatically manage jetbrains dirs

---
+++ utils/create_jetbrains_configs.py
@@ -0,0 +1,42 @@
+import shutil
+
+from pathlib import Path
+
+# Parent directories to create managed config files under.
+TARGET_PATHS = [
+    Path("private_Library/private_Application Support/JetBrains")
+]
+
+# JetBrains editors to create managed configs for. Must be named following the settings folder name format.
+EDITORS = [
+    "GoLand",
+    "IntelliJIdea"
+]
+
+# Editor versions to create configs for.
+VERSIONS = [
+    "2023.3"
+]
+
+# Config files that should be managed by this repo.
+MANAGED_FILES = [
+    "colors.scheme.xml",
+    "editor-font.xml",
+    "ui.lnf.xml",
+]
+
+for target_path in TARGET_PATHS:
+    shutil.rmtree(target_path, ignore_errors=True)
+
+    target_path.mkdir(parents=True, exist_ok=True)
+
+    for editor in EDITORS:
+        for version in VERSIONS:
+            version_path = target_path / f"{editor}{version}" / "options"
+            version_path.mkdir(parents=True, exist_ok=True)
+
+            for managed_file in MANAGED_FILES:
+                managed_file_path = version_path / (managed_file + ".tmpl")
+                managed_file_path.touch(exist_ok=True)
+                managed_file_path.write_text('{{- template "jetbrains/' + managed_file + '" . -}}\n')
+
